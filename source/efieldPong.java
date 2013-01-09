463ece6a06d8b9f2387f0d9b3214a83a5fe62324
ARomadan <nastik@umich.edu>
2013-01-09 01:56:32 -0500

    this is a test, nothing imporatant was modified.

--- source/efieldPong.java
+++ source/efieldPong.java
@@ -15,14 +15,14 @@ import java.util.regex.*;
 public class efieldPong extends PApplet {
 
 /**
-* NAME: Electric Field Pong
-* DESCRIPTION: This is exactly like pong except some of the blocks 
+NAME: Electric Field Pong
+DESCRIPTION: This is exactly like pong except some of the blocks 
                emit electric fields directed radially away from the source, 
                and the ball is a charged particle. When the particle enters 
                the effective radius of an electric field it will feel a force. 
                This force will change the velocity of the particle, thus 
                deflecting the particle from its otherwise straight trajectory.  
-* CONTROLS: 
+CONTROLS: 
    - mouse click to start
    - press the 'a' key on your keybord to accelate the ball
      (you may want to do this when the particle is moving annoyingly slow)
